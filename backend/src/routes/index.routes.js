60ca966b7f7a31f22e249c3ef4bec29c1424f877
Nico <nicolas.opazo2001@alumnos.ubiobio.cl>
2024-10-27 23:40:22 -0300

    MOD index.routes.js

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -3,12 +3,14 @@ import { Router } from "express";
 import userRoutes from "./user.routes.js";
 import authRoutes from "./auth.routes.js";
 import anotacionRoutes from "./anotacion.routes.js"; // Importar las rutas de anotaciones
+import asistencuaRoutes from "./asistencia.routes.js"; // Importar las rutas de asistencia
 
 const router = Router();
 
 router
-    .use("/auth", authRoutes)
-    .use("/user", userRoutes)
-    .use("/anotaciones", anotacionRoutes); // Agregar la ruta para anotaciones
+  .use("/auth", authRoutes)
+  .use("/user", userRoutes)
+  .use("/anotaciones", anotacionRoutes) // Agregar la ruta para anotaciones
+  .use("/asistencia", asistencuaRoutes); // Agregar la ruta para asistencia
 
 export default router;
