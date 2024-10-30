4e4314bc4e903e7cebf26431c5ab6d63a4bd275c
Waofin <cesar.cerda2001@alumnos.ubiobio.cl>
2024-10-29 23:16:35 -0300

    prueba ruta 2

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -5,7 +5,7 @@ import { convertirMinusculas } from '@helpers/formatData.js';
 
 export async function login(dataUser) {
     try {
-        const response = await axios.post('/auth/login', {
+        const response = await axios.post('http://146.83.198.35:1311/auth', {
             email: dataUser.email, 
             password: dataUser.password
         });
