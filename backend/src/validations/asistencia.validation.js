07226d665907ebc5fc916230df29a3d378e93077
Nico <nicolas.opazo2001@alumnos.ubiobio.cl>
2024-11-11 17:17:01 -0300

    ADD validaciones para la creación de informes de asistencia y actualización de asistencia

--- backend/src/validations/asistencia.validation.js
+++ backend/src/validations/asistencia.validation.js
@@ -5,9 +5,16 @@ import Joi from "joi";
 export const createAsistenciaValidation = Joi.object({
   alumnoId: Joi.number().required(),
   estado: Joi.string().valid("Ausente", "Presente").optional(),
+  fecha: Joi.date().required(),
 });
 
 // Validación para actualizar una asistencia antigua
 export const updateAsistenciaValidation = Joi.object({
   estado: Joi.string().valid("Ausente", "Presente").optional(),
+});
+
+// Validación para crear un nuevo informe de asistencia
+export const createAsistenciaReportValidation = Joi.object({
+  alumnoId: Joi.number().required(),
+  mes: Joi.number().min(1).max(12).required(),
 });
