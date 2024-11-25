c9303edc85dc21ea8ae58e65416808176d0d62c3
Nico <nicolas.opazo2001@alumnos.ubiobio.cl>
2024-11-25 18:47:36 -0300

    ADD getCursos endpoint and refactor curso service for improved readability

--- backend/src/controllers/curso.controller.js
+++ backend/src/controllers/curso.controller.js
@@ -1,17 +1,36 @@
 "use strict";
-import { createCursoService, deleteCursoService, updateCursoService } from "../services/curso.service.js";
+import {
+  createCursoService,
+  deleteCursoService,
+  getCursosService,
+  updateCursoService,
+} from "../services/curso.service.js";
 
 // Controlador para crear un curso
 export async function createCurso(req, res) {
   try {
     const { nombre, nivel, año, profesorJefeId } = req.body;
 
-    const [curso, error] = await createCursoService(nombre, nivel, año, profesorJefeId);
+    const [curso, error] = await createCursoService(
+      nombre,
+      nivel,
+      año,
+      profesorJefeId,
+    );
     if (error) return res.status(400).json({ message: error });
 
     res.status(201).json({ message: "Curso creado exitosamente", data: curso });
   } catch (error) {
     res.status(500).json({ message: "Error al crear el curso" });
+  }
+}
+
+export async function getCursos(req, res) {
+  try {
+    const cursos = await getCursosService();
+    res.status(200).json({ data: cursos });
+  } catch (error) {
+    res.status(500).json({ message: "Error al obtener los cursos" });
   }
 }
 
@@ -23,7 +42,9 @@ export async function deleteCurso(req, res) {
     const [curso, error] = await deleteCursoService(id);
     if (error) return res.status(404).json({ message: error });
 
-    res.status(200).json({ message: "Curso eliminado exitosamente", data: curso });
+    res
+      .status(200)
+      .json({ message: "Curso eliminado exitosamente", data: curso });
   } catch (error) {
     res.status(500).json({ message: "Error al eliminar el curso" });
   }
@@ -35,10 +56,17 @@ export async function updateCurso(req, res) {
     const { id } = req.params;
     const { nombre, nivel, año, profesorJefeId } = req.body;
 
-    const [curso, error] = await updateCursoService(id, { nombre, nivel, año, profesorJefeId });
+    const [curso, error] = await updateCursoService(id, {
+      nombre,
+      nivel,
+      año,
+      profesorJefeId,
+    });
     if (error) return res.status(404).json({ message: error });
 
-    res.status(200).json({ message: "Curso modificado exitosamente", data: curso });
+    res
+      .status(200)
+      .json({ message: "Curso modificado exitosamente", data: curso });
   } catch (error) {
     res.status(500).json({ message: "Error al modificar el curso" });
   }
