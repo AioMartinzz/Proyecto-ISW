f17de420325a52cadec663cda27dc2824798e401
Nico <nicolas.opazo2001@alumnos.ubiobio.cl>
2024-11-27 19:08:39 -0300

    Remove logging for cursos response in getCursos function

--- frontend/src/services/curso.service.js
+++ frontend/src/services/curso.service.js
@@ -2,6 +2,5 @@ import axios from './root.service.js'
 
 export const getCursos = async () => {
     const response = await axios.get(`/cursos`)
-    console.log('Response Cursos:', response)
     return response.data
 }
