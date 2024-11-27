1d9b57846b6638885b4871363b8f57802531d257
Nico <nicolas.opazo2001@alumnos.ubiobio.cl>
2024-11-27 19:06:37 -0300

    Add logging for cursos response in getCursos function

--- frontend/src/services/curso.service.js
+++ frontend/src/services/curso.service.js
@@ -2,5 +2,6 @@ import axios from './root.service.js'
 
 export const getCursos = async () => {
     const response = await axios.get(`/cursos`)
+    console.log('Response Cursos:', response)
     return response.data
 }
