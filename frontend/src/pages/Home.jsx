52852af02d4dd6daf2952bc8fc49b701e73319a6
Waofin <cesar.cerda2001@alumnos.ubiobio.cl>
2024-12-17 13:26:11 -0300

    Cards inicio

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,28 +5,36 @@ const Home = () => {
     return (
         <div className="home-container">
             <header className="home-header">
-                <h1>Bienvenido a la Aplicación</h1>
+                <h1>Bienvenido al Sistema Integrado de Gestión Académica</h1>
                 <p>
                     Explora nuestras funcionalidades y comienza a usar la
                     aplicación.
                 </p>
             </header>
-            <section className="home-features">
-                <div className="feature">
-                    <h2>Funcionalidad 1</h2>
-                    <p>Descripción breve de la funcionalidad 1.</p>
+            <div className="home-features">
+                <div className="feature-card">
+                    <div className="feature-card-content">
+                        <h2>Registro de Usuarios</h2>
+                        <p>Descripción breve de la funcionalidad 1.</p>
+                    </div>
                 </div>
-                <div className="feature">
-                    <h2>Funcionalidad 2</h2>
-                    <p>Digitalización de la asistencia.</p>
-                    <p>Generación de informes de asistencia.</p>
-                    <p>Envío de aviso por correo electrónico.</p>
+                <div className="feature-card">
+                    <div className="feature-card-content">
+                        <h2>Portal de Asistencia 2</h2>
+                        <p>Digitalización de la asistencia.</p>
+                        <p>Generación de informes de asistencia.</p>
+                        <p>Envío de aviso por correo electrónico.</p>
+                    </div>
                 </div>
-                <div className="feature">
-                    <h2>Funcionalidad 3</h2>
-                    <p>Descripción breve de la funcionalidad 3.</p>
+                <div className="feature-card">
+                    <div className="feature-card-content">
+                        <h2>Portal de Notas</h2>
+                        <p>Digitalización de las notas de los estudiantes.</p>
+                        <p>Estadisticas de las notas.</p>
+                       
+                    </div>
                 </div>
-            </section>
+            </div>
             <footer className="home-footer">
                 <p>
                     © 2024 Ingeniería de Software. Todos los derechos
